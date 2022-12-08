1602eb96b985a66cdfaa4134b5b186b160b38b38
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2022-12-08 19:59:15 -0300

    Estrutura do verso da ficha quase pronta.

--- fisiologia/clonagem-header.js
+++ fisiologia/clonagem-header.js
@@ -3,8 +3,8 @@
 
 
 window.addEventListener("load", () => {
-    const cabecalho_da_ficha = document.querySelector("div.page1 header.cabecalho-da-ficha");
-    const verso_da_ficha = document.querySelector("div.page2");
+    const cabecalho_da_ficha = document.querySelector("div.ficha.fronte header.cabecalho-da-ficha");
+    const verso_da_ficha = document.querySelector("div.ficha.verso");
 
     const clone = cabecalho_da_ficha.cloneNode(true);
     verso_da_ficha.insertAdjacentElement("afterbegin", clone);
