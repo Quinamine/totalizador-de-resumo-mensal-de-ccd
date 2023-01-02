8f9c8ac48bac58cd1d2ced418fa2e57cb54531c4
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2023-01-02 09:12:19 -0300

    Ready to host.

--- fisiologia/clonagem-header.js
+++ fisiologia/clonagem-header.js
@@ -10,8 +10,21 @@ window.addEventListener("load", () => {
     verso_da_ficha.insertAdjacentElement("beforebegin", cabecalho_principal.cloneNode(true));
     verso_da_ficha.insertAdjacentElement("afterbegin", cabecalho_da_ficha.cloneNode(true));
 
-    // Omissão do cabecalho do verso da ficha
-    const cabecalho_da_fichaVerso =  document.querySelectorAll("div.container > header")[1];
-    cabecalho_da_fichaVerso.classList.add("hidden");
+    // Omissão do cabeçalho do verso da ficha
+    const cabecalho_fichaVerso =  document.querySelectorAll("div.container > header")[1];
+    //cabecalho_fichaVerso.classList.add("hidden");
 
-})
+
+    // Cópia dos dados inseridos nas células de entrada do cabeçalho 
+    const campos_do_cabecalho_fichaVerso =
+    document.querySelectorAll("div.container > header")[1].querySelectorAll("input");   
+
+    // Usei um setTimeout no evento de impressão no arquivo do `menu.js` para que a função abaixo seja executada antes da impressão
+    const btnImprimir = document.querySelector("button.imprimir");
+    btnImprimir.addEventListener("click", () => {
+        for (let i=0; i<campos_do_cabecalho_fichaVerso.length; i++) {  
+            campos_do_cabecalho_fichaVerso[i].value = localStorage.getItem(`trmccd-${campos_do_cabecalho_fichaVerso[i].id}`);
+        }
+    });
+
+});
